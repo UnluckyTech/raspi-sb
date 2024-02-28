e8ad3e4c5b7eb3b3cff6658c1ce67d6b9dc27f98
UnluckyTech <stawse99@protonmail.com>
2024-02-27 21:42:59 -0500

    fixed pass insert to pihole

--- .install/pihole.sh
+++ .install/pihole.sh
@@ -3,11 +3,10 @@ echo "Installing Pihole"
 cp -rf ~/raspi-sb/pihole/ ~/ 
 cd ~/pihole
 compose_file=~/pihole/docker-compose.yml
-
-pi_password=$(openssl rand -base64 12 | tr -dc 'a-zA-Z0-9' | head -c 12)
 # Create password to access pihole web interface.
-# read -p "Enter the desired password: " user_password
-sed -i "s/WEBPASSWORD=.*/WEBPASSWORD=$pi_password/" "$compose_file"
+user_password=$(pwgen -s 12 1)
+sed -i "s/WEBPASSWORD: 'set a secure password here or it will be random'/WEBPASSWORD: '$user_password'/" "$compose_file"
+echo "Password updated successfully!"
 sudo docker-compose up -d
 cd ~/raspi-sb
 if sudo docker ps | grep -q "pihole"; then
